39074f448831c1b3d303f14fa4b60d76109ee22c
harsh3294 <harshc3294@gmail.com>
2021-08-06 11:43:56 +0530

    updated the code of factorial and coffee shop

---
+++ 02/HelloWorld/HelloWorld/Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Factorial
+    {
+       internal static int calculateFactorialUsingRecursion(int number)
+        {
+
+            if (number == 0) return 1;
+            else if (number == 1) return number;
+            else if (number < 0) return 0;
+            
+            return number * calculateFactorialUsingRecursion(number - 1);
+        }
+        internal static int calculateFactorialUsingForLoop(int number)
+        {
+            int result = 1;
+            if (number == 0) return 1;
+            else if(number < 0)    return 0;
+  
+            for (int i = number; i >= 1; i--) result *= i;
+            return result;
+        }
+    }
+}
---
+++ 02/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter number to calculate the factorial of numnber");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //if (Factorial.calculateFactorialUsingRecursion(userInput) == 0) Console.WriteLine("Invalid Input");
+            //else Console.WriteLine("Calculating Factorial using Recursive : = " + Factorial.calculateFactorialUsingRecursion(userInput));
+            //if (Factorial.calculateFactorialUsingForLoop(userInput) == 0) Console.WriteLine("Invalid Input");
+            //else Console.WriteLine("Calculating Factorial using For Loop : = " + Factorial.calculateFactorialUsingForLoop(userInput));
+
+            Console.WriteLine("---------------------Welcome to Cafe Coffee Day---------------------");
+            Console.WriteLine("Small Coffee  Rs.25 \nMedium Coffee Rs.40 \nLargeCoffee Rs.60");
+            Console.WriteLine("Press <1> for small coffee \nPress <2> for medium coffee \nPress <3> for large coffee");
+            Console.WriteLine("Enter your selection");
+            string userSelection=Console.ReadLine();
+            Console.WriteLine("Enter the Quantity : ");
+            int quantity=Convert.ToInt32(Console.ReadLine());
+            CoffeeMachine.getCoffee(userSelection, quantity);
+        }
+    }
+}
