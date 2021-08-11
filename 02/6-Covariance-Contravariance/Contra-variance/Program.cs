6b80f84c0041a0e49151a149b9e8b035ea46b62a
aman1862000 <rjamans18@gmail.com>
2021-08-11 09:48:15 +0530

    team 6 added assignment

---
+++ 02/6-Covariance-Contravariance/Contra-variance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Contra_variance
+{
+   delegate Small covarDel(Big mc);
+
+   public class Small
+    {   
+    }
+    public class Big: Small
+    {
+    }
+    public class Bigger : Big
+    { 
+    }
+
+    class Program
+    {
+        static Big Method1(Big bg)
+        {
+            Console.WriteLine("Method1");
+            return new Big();
+        }
+        static Small Method2(Big bg)
+        {
+            Console.WriteLine("Method2");
+            return new Small();
+        }
+
+        static Small Method3(Small sml)
+        {
+            Console.WriteLine("Method3");
+            return new Small();
+        }
+        static void Main(string[] args)
+        {
+            covarDel del = Method1;
+            del += Method2;
+            del += Method3;
+
+            Small sm = del(new Big());
+        }
+    }
+}
