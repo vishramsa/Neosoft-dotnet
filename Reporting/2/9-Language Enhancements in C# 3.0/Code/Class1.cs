5f1569dd0f5ef20d718e0b74a4b7e32f6e351279
akshay9967 <akshay3708@gmail.com>
2021-08-23 21:34:59 +0530

    9-Language Enhancements in C# 3.0

---
+++ Reporting/2/9-Language Enhancements in C# 3.0/Code/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public string Display()
+        {
+            return ("I m in Display");
+        }
+
+        public string Print()
+        {
+            return ("I m in Print");
+        }
+    }
+}
---
+++ Reporting/2/9-Language Enhancements in C# 3.0/Code/CollectionCode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+    class ObjectInitializer
+    {
+        public static void ColCode()
+        {
+            List<Student> students = new List<Student> {
+                new Student { ID=101, Name="Rahul", Email="rahul@example.com" },
+                new Student { ID=102, Name="Peter", Email="peter@abc.com" },
+                new Student { ID=103, Name="Irfan", Email="irfan@example.com" }
+            };
+            foreach (Student student in students)
+            {
+                Console.Write(student.ID + " ");
+                Console.Write(student.Name + " ");
+                Console.Write(student.Email + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
